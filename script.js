74f0abcee36ddcffccefa9c56a8fa8b671aca89b
Nick <nsardonini@gmail.com>
2020-02-26 17:34:53 -0500

    added last of event listeners for digits, decimal, and clear

--- script.js
+++ script.js
@@ -25,8 +25,6 @@ function operate(oper, a, b) {
     }
 }
 
-let display = document.getElementById('display').innerText;
-
 let nums = {};
 for (let i = 0; i < 10; i++) {
     nums[i] = i;
@@ -43,11 +41,44 @@ decimal.addEventListener('click', () => {
     if (search == -1) {
         document.getElementById('display').innerText += '.';
     };
-    console.log(search);
 });
-
 const one = document.getElementById('one');
 one.addEventListener('click', () => {
-    document.getElementById('display').innerText += '1';
-    console.log(document.getElementById('display').innerText);
+    document.getElementById('display').innerText += 1;
+});
+const two = document.getElementById('two');
+two.addEventListener('click', () => {
+    document.getElementById('display').innerText += 2;
+});
+const three = document.getElementById('three');
+three.addEventListener('click', () => {
+    document.getElementById('display').innerText += 3;
+});
+const four = document.getElementById('four');
+four.addEventListener('click', () => {
+    document.getElementById('display').innerText += 4;
+});
+const five = document.getElementById('five');
+five.addEventListener('click', () => {
+    document.getElementById('display').innerText += 5;
+});
+const six = document.getElementById('six');
+six.addEventListener('click', () => {
+    document.getElementById('display').innerText += 6;
+});
+const seven = document.getElementById('seven');
+seven.addEventListener('click', () => {
+    document.getElementById('display').innerText += 7;
+});
+const eight = document.getElementById('eight');
+eight.addEventListener('click', () => {
+    document.getElementById('display').innerText += 8;
+});
+const nine = document.getElementById('nine');
+nine.addEventListener('click', () => {
+    document.getElementById('display').innerText += 9;
+});
+const zero = document.getElementById('zero');
+zero.addEventListener('click', () => {
+    document.getElementById('display').innerText += 0;
 });
