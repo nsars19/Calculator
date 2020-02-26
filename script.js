144e770947dad4dd8132146fba43eb3e653c49d8
Nick <nsardonini@gmail.com>
2020-02-26 17:26:47 -0500

    added button functionality to some buttons. removed p element & styling assigned to it

--- script.js
+++ script.js
@@ -24,5 +24,30 @@ function operate(oper, a, b) {
         return divide(a, b);
     }
 }
-const buttonContainer = document.getElementById('buttonContainer');
-const button = document.createElement('button');
+
+let display = document.getElementById('display').innerText;
+
+let nums = {};
+for (let i = 0; i < 10; i++) {
+    nums[i] = i;
+};
+
+const clearDisplay = document.getElementById('clear');
+clearDisplay.addEventListener('click', () => {
+    document.getElementById('display').innerText = '';
+});
+const decimal = document.getElementById('decimal');
+decimal.addEventListener('click', () => {
+    let displayValue = document.getElementById('display').innerText;
+    let search = displayValue.search(/[.]/);
+    if (search == -1) {
+        document.getElementById('display').innerText += '.';
+    };
+    console.log(search);
+});
+
+const one = document.getElementById('one');
+one.addEventListener('click', () => {
+    document.getElementById('display').innerText += '1';
+    console.log(document.getElementById('display').innerText);
+});
