a8155732d5a8ff76c35b38d293cb1ff4222ac0aa
Nick <nsardonini@gmail.com>
2020-02-27 14:28:26 -0500

    reformatted DOM elements into variables for better readability

--- script.js
+++ script.js
@@ -26,94 +26,99 @@ function operate(oper, a, b) {
 }
 
 // Event listeners for digits, decimal, and clear
+const displayVal = document.getElementById('display');
 
 const clearDisplay = document.getElementById('clear');
 clearDisplay.addEventListener('click', () => {
-    document.getElementById('display').innerText = '';
+    displayVal.innerText = '';
     delete value['firstNum'];
     delete value['secondNum'];
     delete value['operator'];
     delete value['total'];
 });
-const decimal = document.getElementById('decimal');
-decimal.addEventListener('click', () => {
-    if (document.getElementById('display').innerText.search(/[.]/) == -1) {
-        document.getElementById('display').innerText += '.';
+const decimalElem = document.getElementById('decimal');
+decimalElem.addEventListener('click', () => {
+    if (displayVal.innerText.search(/[.]/) == -1) {
+        displayVal.innerText += '.';
     };
 });
-const one = document.getElementById('one');
-one.addEventListener('click', () => {
-    document.getElementById('display').innerText += 1;
+const oneElem = document.getElementById('one');
+oneElem.addEventListener('click', () => {
+    displayVal.innerText += 1;   
 });
-const two = document.getElementById('two');
-two.addEventListener('click', () => {
-    document.getElementById('display').innerText += 2;
+const twoElem = document.getElementById('two');
+twoElem.addEventListener('click', () => {
+    displayVal.innerText += 2;
 });
-const three = document.getElementById('three');
-three.addEventListener('click', () => {
-    document.getElementById('display').innerText += 3;
+const threeElem = document.getElementById('three');
+threeElem.addEventListener('click', () => {
+    displayVal.innerText += 3;
 });
-const four = document.getElementById('four');
-four.addEventListener('click', () => {
-    document.getElementById('display').innerText += 4;
+const fourElem = document.getElementById('four');
+fourElem.addEventListener('click', () => {
+    displayVal.innerText += 4;
 });
-const five = document.getElementById('five');
-five.addEventListener('click', () => {
-    document.getElementById('display').innerText += 5;
+const fiveElem = document.getElementById('five');
+fiveElem.addEventListener('click', () => {
+    displayVal.innerText += 5;
 });
-const six = document.getElementById('six');
-six.addEventListener('click', () => {
-    document.getElementById('display').innerText += 6;
+const sixElem = document.getElementById('six');
+sixElem.addEventListener('click', () => {
+    displayVal.innerText += 6;
 });
-const seven = document.getElementById('seven');
-seven.addEventListener('click', () => {
-    document.getElementById('display').innerText += 7;
+const sevenElem = document.getElementById('seven');
+sevenElem.addEventListener('click', () => {
+    displayVal.innerText += 7;
 });
-const eight = document.getElementById('eight');
-eight.addEventListener('click', () => {
-    document.getElementById('display').innerText += 8;
+const eightElem = document.getElementById('eight');
+eightElem.addEventListener('click', () => {
+    displayVal.innerText += 8;
 });
-const nine = document.getElementById('nine');
-nine.addEventListener('click', () => {
-    document.getElementById('display').innerText += 9;
+const nineElem = document.getElementById('nine');
+nineElem.addEventListener('click', () => {
+    displayVal.innerText += 9;
 });
-const zero = document.getElementById('zero');
-zero.addEventListener('click', () => {
-    document.getElementById('display').innerText += 0;
+const zeroElem = document.getElementById('zero');
+zeroElem.addEventListener('click', () => {
+    displayVal.innerText += 0;
 });
-
-// Object to hold input values & chosen operator
-
-let value = {};
 
 // Event listeners for operators
 
 const minusOp = document.getElementById('subtract');
 minusOp.addEventListener('click', () => {
     if ('firstNum' in value) {
-
+        value['secondNum'] = displayVal.innerText;
+        displayVal.innerText = subtract(value['firstNum'], value['secondNum']);
+        value['total'] = displayVal.innerText;
     }
-    value['firstNum'] = document.getElementById('display').innerText;
+    if ('total' in value) {
+        displayVal.innerText = 0;
+        displayVal.innerText = subtract(display['total'], display['secondNum']);
+    } else {
+        value['firstNum'] = displayVal.innerText;
     value['operator'] = 'subtract';
-    document.getElementById('display').innerText = '';
+    displayVal.innerText = '';
+    console.log('a');
+}
 });
 const addOp = document.getElementById('add');
 addOp.addEventListener('click', () => {
-    value['firstNum'] = document.getElementById('display').innerText;
+    value['firstNum'] = displayVal.innerText;
     value['operator'] = 'add';
-    document.getElementById('display').innerText = '';
+    displayVal.innerText = '';
 });
 const multiplyOp = document.getElementById('multiply');
 multiplyOp.addEventListener('click', () => {
-    value['firstNum'] = document.getElementById('display').innerText;
+    value['firstNum'] = displayVal.innerText;
     value['operator'] = 'multiply';
-    document.getElementById('display').innerText = '';
+    displayVal.innerText = '';
 });
 const divideOp = document.getElementById('divide');
 divideOp.addEventListener('click', () => {
-    value['firstNum'] = document.getElementById('display').innerText;
+    value['firstNum'] = displayVal.innerText;
     value['operator'] = 'divide';
-    document.getElementById('display').innerText = '';
+    displayVal.innerText = '';
 });
 
 // Event listener for return total
@@ -121,17 +126,20 @@ divideOp.addEventListener('click', () => {
 const enter = document.getElementById('enter');
 enter.addEventListener('click', () => {
     if ('firstNum' in value === false) {
-        document.getElementById('display') = '';
+        displayVal.innerText = '';
     }
     if ('total' in value) {
-        value['secondNum'] = document.getElementById('display').innerText;
-        document.getElementById('display').innerText = operate(value['operator'], Number(value['total']), Number(value['secondNum']));
-        value['total'] = document.getElementById('display').innerText;
-
+        value['secondNum'] = displayVal.innerText;
+        displayVal.innerText = operate(value['operator'], Number(value['total']), Number(value['secondNum']));
+        value['total'] = displayVal.innerText;
+        
     } else {
-    value['secondNum'] = document.getElementById('display').innerText;
-    document.getElementById('display').innerText = operate(value['operator'], Number(value['firstNum']), Number(value['secondNum']));
-    value['total'] = document.getElementById('display').innerText;
+        value['secondNum'] = displayVal.innerText;
+        displayVal.innerText = operate(value['operator'], Number(value['firstNum']), Number(value['secondNum']));
+        value['total'] = displayVal.innerText;
     }
 });
 
+// Object to hold input values & chosen operator
+
+let value = {};
