d20aaab1d70214b4d72763cb9ff4729d64d34025
Nick <nsardonini@gmail.com>
2020-02-27 20:08:28 -0500

    removed object storage. replaced with variables. re-coded event listeners

--- script.js
+++ script.js
@@ -24,17 +24,16 @@ function operate(oper, a, b) {
         return divide(a, b);
     }
 }
-
-// Event listeners for digits, decimal, and clear
+// Variable for storing the value of the display
 const displayVal = document.getElementById('display');
 
 const clearDisplay = document.getElementById('clear');
 clearDisplay.addEventListener('click', () => {
     displayVal.innerText = '';
-    delete value['firstNum'];
-    delete value['secondNum'];
-    delete value['operator'];
-    delete value['total'];
+    firstVar = undefined;
+    secondVar = undefined;
+    totalVar = undefined;
+    operatorVar = undefined;
 });
 const decimalElem = document.getElementById('decimal');
 decimalElem.addEventListener('click', () => {
@@ -42,93 +41,100 @@ decimalElem.addEventListener('click', () => {
         displayVal.innerText += '.';
     };
 });
-const oneElem = document.getElementById('one');
-oneElem.addEventListener('click', () => {
-    displayVal.innerText += 1;   
-});
-const twoElem = document.getElementById('two');
-twoElem.addEventListener('click', () => {
-    displayVal.innerText += 2;
-});
-const threeElem = document.getElementById('three');
-threeElem.addEventListener('click', () => {
-    displayVal.innerText += 3;
-});
-const fourElem = document.getElementById('four');
-fourElem.addEventListener('click', () => {
-    displayVal.innerText += 4;
-});
-const fiveElem = document.getElementById('five');
-fiveElem.addEventListener('click', () => {
-    displayVal.innerText += 5;
-});
-const sixElem = document.getElementById('six');
-sixElem.addEventListener('click', () => {
-    displayVal.innerText += 6;
-});
-const sevenElem = document.getElementById('seven');
-sevenElem.addEventListener('click', () => {
-    displayVal.innerText += 7;
-});
-const eightElem = document.getElementById('eight');
-eightElem.addEventListener('click', () => {
-    displayVal.innerText += 8;
-});
-const nineElem = document.getElementById('nine');
-nineElem.addEventListener('click', () => {
-    displayVal.innerText += 9;
-});
-const zeroElem = document.getElementById('zero');
-zeroElem.addEventListener('click', () => {
-    displayVal.innerText += 0;
-});
 
-// Event listeners for operators
+for (let i = 0; i < 10; i++) { // Creates event listeners for digit buttons
+    document.getElementById(`${i}`).addEventListener('click', () => {
+        displayVal.innerText += i;
+    });
+}
+
+// Event listeners for operators & variables to store input values
+
+let firstVar, 
+    secondVar,
+    totalVar,
+    operatorVar = '';
 
 const minusOp = document.getElementById('subtract');
 minusOp.addEventListener('click', () => {
-    value['firstNum'] = displayVal.innerText;
-    value['operator'] = 'subtract';
+    operatorVar = 'subtract';
+    if (firstVar === undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    } else if (secondVar === undefined) {
+        secondVar = displayVal.innerText;
+        displayVal.innerText = '';
+    }
+    if (firstVar !== undefined && secondVar !== undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    }
     displayVal.innerText = '';
+    
 });
 const addOp = document.getElementById('add');
 addOp.addEventListener('click', () => {
-    value['firstNum'] = displayVal.innerText;
-    value['operator'] = 'add';
-    displayVal.innerText = '';
-});
-const multiplyOp = document.getElementById('multiply');
-multiplyOp.addEventListener('click', () => {
-    value['firstNum'] = displayVal.innerText;
-    value['operator'] = 'multiply';
-    displayVal.innerText = '';
-});
-const divideOp = document.getElementById('divide');
-divideOp.addEventListener('click', () => {
-    value['firstNum'] = displayVal.innerText;
-    value['operator'] = 'divide';
-    displayVal.innerText = '';
-});
-
-// Event listener for return total
-
-const enter = document.getElementById('enter');
-enter.addEventListener('click', () => {
-    if ('firstNum' in value === false) {
+    operatorVar = 'add';
+    if (firstVar === undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    } else if (secondVar === undefined) {
+        secondVar = displayVal.innerText;
         displayVal.innerText = '';
     }
-    if ('total' in value) {
-        value['secondNum'] = displayVal.innerText;
-        displayVal.innerText = operate(value['operator'], Number(value['total']), Number(value['secondNum']));
-        value['total'] = displayVal.innerText;
-        
-    } else {
-        value['secondNum'] = displayVal.innerText;
-        displayVal.innerText = operate(value['operator'], Number(value['firstNum']), Number(value['secondNum']));
-        value['total'] = displayVal.innerText;
+    if (firstVar !== undefined && secondVar !== undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
     }
+    displayVal.innerText = '';
+    displayVal.innerText = '';
 });
 
-// Object to hold input values & chosen operator
+const multiplyOp = document.getElementById('multiply');
+multiplyOp.addEventListener('click', () => {
+    operatorVar = 'multiply';
+    if (firstVar === undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    } else if (secondVar === undefined) {
+        secondVar = displayVal.innerText;
+        displayVal.innerText = '';
+    }
+    if (firstVar !== undefined && secondVar !== undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    }
+    displayVal.innerText = '';
+});
 
-let value = {};
+const divideOp = document.getElementById('divide');
+divideOp.addEventListener('click', () => {
+    operatorVar = 'divide';
+    if (firstVar === undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    } else if (secondVar === undefined) {
+        secondVar = displayVal.innerText;
+        displayVal.innerText = '';
+    }
+    if (firstVar !== undefined && secondVar !== undefined) {
+        firstVar = displayVal.innerText;
+        displayVal.innerText = '';
+    }
+    displayVal.innerText = '';
+});
+
+// Event listener for equals button
+const enter = document.getElementById('enter');
+enter.addEventListener('click', () => {
+    if (firstVar === undefined && secondVar === undefined) {
+        displayVal.innerText = '';
+    } else if (firstVar !== undefined && secondVar !== undefined) {
+        secondVar = displayVal.innerText;
+        displayVal.innerText = operate(operatorVar, firstVar, secondVar);
+    } else {
+        secondVar = displayVal.innerText;
+        displayVal.innerText = operate(operatorVar, firstVar, secondVar);
+    }    
+});
+
