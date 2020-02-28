c0ef4ab2ff8d1d84094effa8e28de76b30fa34db
Nick <nsardonini@gmail.com>
2020-02-27 20:53:21 -0500

    added keyboard functionality

--- script.js
+++ script.js
@@ -137,4 +137,127 @@ enter.addEventListener('click', () => {
         displayVal.innerText = operate(operatorVar, firstVar, secondVar);
     }    
 });
-
+// Keyboard functionality || same code as above, but attached to
+// corresponding keys
+document.onkeydown = function(event) {
+    if (event.keyCode == 97 || event.keyCode == 49) {
+        displayVal.innerText += 1;
+    }
+    if (event.keyCode == 98 || event.keyCode == 50) {
+        displayVal.innerText += 2;
+    } 
+    if (event.keyCode == 99 || event.keyCode == 51) {
+        displayVal.innerText += 3;
+    }
+    if (event.keyCode == 100 || event.keyCode == 52) {
+        displayVal.innerText += 4;
+    }
+    if (event.keyCode == 101 || event.keyCode == 53) {
+        displayVal.innerText += 5;
+    }
+    if (event.keyCode == 102 || event.keyCode == 54) {
+        displayVal.innerText += 6;
+    }
+    if (event.keyCode == 103 || event.keyCode == 55) {
+        displayVal.innerText += 7;
+    }
+    if (event.keyCode == 104 || event.keyCode == 56) {
+        displayVal.innerText += 8;
+    }
+    if (event.keyCode == 105 || event.keyCode == 57) {
+        displayVal.innerText += 9;
+    }
+    if (event.keyCode == 96 || event.keyCode == 48) {
+        displayVal.innerText += 0;
+    }
+    if (event.keyCode == 35 || event.keyCode == 49) {
+        displayVal.innerText += 1;
+    }
+    if (event.keyCode == 107) {
+        operatorVar = 'add';
+        if (firstVar === undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        } else if (secondVar === undefined) {
+            secondVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        if (firstVar !== undefined && secondVar !== undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        displayVal.innerText = '';
+        displayVal.innerText = '';
+    }
+    if (event.keyCode == 109) {
+        operatorVar = 'subtract';
+        if (firstVar === undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        } else if (secondVar === undefined) {
+            secondVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        if (firstVar !== undefined && secondVar !== undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        displayVal.innerText = '';
+        displayVal.innerText = '';
+    }
+    if (event.keyCode == 106) {
+        operatorVar = 'multiply';
+        if (firstVar === undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        } else if (secondVar === undefined) {
+            secondVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        if (firstVar !== undefined && secondVar !== undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        displayVal.innerText = '';
+        displayVal.innerText = '';
+    }
+    if (event.keyCode == 111) {
+        operatorVar = 'divide';
+        if (firstVar === undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        } else if (secondVar === undefined) {
+            secondVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        if (firstVar !== undefined && secondVar !== undefined) {
+            firstVar = displayVal.innerText;
+            displayVal.innerText = '';
+        }
+        displayVal.innerText = '';
+        displayVal.innerText = '';
+    }
+    if (event.keyCode == 13) {
+        if (firstVar === undefined && secondVar === undefined) {
+            displayVal.innerText = '';
+        } else if (firstVar !== undefined && secondVar !== undefined) {
+            secondVar = displayVal.innerText;
+            displayVal.innerText = operate(operatorVar, firstVar, secondVar);
+        } else {
+            secondVar = displayVal.innerText;
+            displayVal.innerText = operate(operatorVar, firstVar, secondVar);
+        } 
+    }
+    if (event.keyCode == 110) {
+        if (displayVal.innerText.search(/[.]/) == -1) {
+            displayVal.innerText += '.';
+        };
+    }   
+    if (event.keyCode == 8) {
+        displayVal.innerText = '';
+        firstVar = undefined;
+        secondVar = undefined;
+        totalVar = undefined;
+        operatorVar = undefined;
+    }
+};
