fd0fdb20a298f49b4686d54e387399cc01380dfa
Nick <nsardonini@gmail.com>
2020-02-28 16:27:53 -0500

    added parseFloat to add function so it would stop concatenating the result

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const add = (a, b) => { return a + b; }
+const add = (a, b) => { return parseFloat(a) + parseFloat(b); }
 const subtract = (a, b) => { return a - b; }
 const multiply = (a, b) => { return a * b; }
 const divide = (a, b) => { return a / b; }
