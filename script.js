78527b14e60197ca19db318c90c47932b861b989
Nick <nsardonini@gmail.com>
2020-03-03 18:36:07 -0500

    changed equals to return as a float only if the answer is a float. refactored keyboard functionality

--- script.js
+++ script.js
@@ -25,6 +25,9 @@ const operate = (oper, a, b) => {
 const displayVal = document.getElementById('display');
 const displayTop = document.getElementById('topText');
 
+
+// -------- Event listeners for Clear, Decimal, and Digit keys ---------- //
+
 const clearDisplay = document.getElementById('clear');
 clearDisplay.addEventListener('click', () => {
     displayVal.innerText = '';
@@ -93,18 +96,16 @@ divideOp.addEventListener('click', () => {
 
 const enter = document.getElementById('enter');
 enter.addEventListener('click', () => {
-    const expArr ={}; // Object to hold expressions
     const textArr = displayTop.innerText.split(/([^0-9])/g);
 
     main:
     while (textArr.length > 1) {
-
+    // Performs equations inside the array. Splices result into array,
+    // and removes the empty indices
         for (let i = 0; i < textArr.length; i++) {
-
             if (textArr[i] === '/' || textArr[i] === '*') {
                 let total = operate(textArr[i], parseFloat(textArr[i - 1]), parseFloat(textArr[i + 1]));
                 textArr.splice((i - 1), 3, total);
-                console.log(textArr);
                 continue main;
 
             } else if (textArr[i] === '+' || textArr[i] === '-') {
@@ -117,15 +118,18 @@ enter.addEventListener('click', () => {
             if (textArr[i] === '+' || textArr[i] === '-') {
                 let total = operate(textArr[i], parseFloat(textArr[i - 1]), parseFloat(textArr[i + 1]));
                 textArr.splice((i - 1), 3, total);
-                console.log(textArr);
                 continue main;
 
             } else if (textArr[i] === '/' || textArr[i] === '*') {
                 continue;
             }
         }
+    }
+    if (textArr.join('').includes('.')) { 
+        displayVal.innerText = textArr[0].toFixed(6);
+    } else {
+        displayVal.innerText = parseInt(textArr[0]);
     }    
-    displayVal.innerText = textArr[0];
 });
 
 
@@ -133,110 +137,77 @@ enter.addEventListener('click', () => {
 //--------- same code as above, but attached to corresponding keys ------ //
 
 document.onkeydown = function(event) {
-    if (event.keyCode == 97 || event.keyCode == 49) {
-        displayVal.innerText += 1;
-    }
-    if (event.keyCode == 98 || event.keyCode == 50) {
-        displayVal.innerText += 2;
-    } 
-    if (event.keyCode == 99 || event.keyCode == 51) {
-        displayVal.innerText += 3;
-    }
-    if (event.keyCode == 100 || event.keyCode == 52) {
-        displayVal.innerText += 4;
-    }
-    if (event.keyCode == 101 || event.keyCode == 53) {
-        displayVal.innerText += 5;
-    }
-    if (event.keyCode == 102 || event.keyCode == 54) {
-        displayVal.innerText += 6;
-    }
-    if (event.keyCode == 103 || event.keyCode == 55) {
-        displayVal.innerText += 7;
-    }
-    if (event.keyCode == 104 || event.keyCode == 56) {
-        displayVal.innerText += 8;
-    }
-    if (event.keyCode == 105 || event.keyCode == 57) {
-        displayVal.innerText += 9;
-    }
-    if (event.keyCode == 96 || event.keyCode == 48) {
-        displayVal.innerText += 0;
+    for (let i = 96; i <= 105; i++) {
+        if (event.keyCode == i || event.keyCode == i - 48) {
+            displayVal.innerText += `${i - 96}`;
+            displayTop.innerText += `${i - 96}`;
+        }
     }
     if (event.keyCode == 107) {
-        operatorVar = 'add';
-        if (firstVar === undefined) {
-            firstVar = displayVal.innerText;
+        if (displayTop.innerText.lastIndexOf('+') !== displayTop.innerText.length - 1) {
+            displayTop.innerText += `+`;
             displayVal.innerText = '';
-        } else if (secondVar === undefined) {
-            secondVar = displayVal.innerText;
-            displayVal.innerText = '';
+        } else {
+            displayTop.innerText = displayTop.innerText;
         }
-        if (firstVar !== undefined && secondVar !== undefined) {
-            firstVar = displayVal.innerText;
-            displayVal.innerText = '';
-        }
-        displayVal.innerText = '';
-        displayVal.innerText = '';
     }
     if (event.keyCode == 109) {
-        operatorVar = 'subtract';
-        if (firstVar === undefined) {
-            firstVar = displayVal.innerText;
+        if (displayTop.innerText.lastIndexOf('-') !== displayTop.innerText.length - 1) {
+            displayTop.innerText += `-`;
             displayVal.innerText = '';
-        } else if (secondVar === undefined) {
-            secondVar = displayVal.innerText;
-            displayVal.innerText = '';
+        } else {
+            displayTop.innerText = displayTop.innerText;
         }
-        if (firstVar !== undefined && secondVar !== undefined) {
-            firstVar = displayVal.innerText;
-            displayVal.innerText = '';
-        }
-        displayVal.innerText = '';
-        displayVal.innerText = '';
     }
     if (event.keyCode == 106) {
-        operatorVar = 'multiply';
-        if (firstVar === undefined) {
-            firstVar = displayVal.innerText;
+        if (displayTop.innerText.lastIndexOf('*') !== displayTop.innerText.length - 1) {
+            displayTop.innerText += `*`;
             displayVal.innerText = '';
-        } else if (secondVar === undefined) {
-            secondVar = displayVal.innerText;
-            displayVal.innerText = '';
+        } else {
+            displayTop.innerText = displayTop.innerText;
         }
-        if (firstVar !== undefined && secondVar !== undefined) {
-            firstVar = displayVal.innerText;
-            displayVal.innerText = '';
-        }
-        displayVal.innerText = '';
-        displayVal.innerText = '';
     }
     if (event.keyCode == 111) {
-        operatorVar = 'divide';
-        if (firstVar === undefined) {
-            firstVar = displayVal.innerText;
+        if (displayTop.innerText.lastIndexOf('/') !== displayTop.innerText.length - 1) {
+            displayTop.innerText += `/`;
             displayVal.innerText = '';
-        } else if (secondVar === undefined) {
-            secondVar = displayVal.innerText;
-            displayVal.innerText = '';
+        } else {
+            displayTop.innerText = displayTop.innerText;
         }
-        if (firstVar !== undefined && secondVar !== undefined) {
-            firstVar = displayVal.innerText;
-            displayVal.innerText = '';
-        }
-        displayVal.innerText = '';
-        displayVal.innerText = '';
     }
     if (event.keyCode == 13) {
-        if (firstVar === undefined && secondVar === undefined) {
-            displayVal.innerText = '';
-        } else if (firstVar !== undefined && secondVar !== undefined) {
-            secondVar = displayVal.innerText;
-            displayVal.innerText = operate(operatorVar, firstVar, secondVar);
-        } else {
-            secondVar = displayVal.innerText;
-            displayVal.innerText = operate(operatorVar, firstVar, secondVar);
-        } 
+        const textArr = displayTop.innerText.split(/([^0-9])/g);
+
+        main:
+        while (textArr.length > 1) {
+            
+            for (let i = 0; i < textArr.length; i++) {
+    
+                if (textArr[i] === '/' || textArr[i] === '*') {
+                    let total = operate(textArr[i], parseFloat(textArr[i - 1]), parseFloat(textArr[i + 1]));
+                    textArr.splice((i - 1), 3, total);
+                    console.log(textArr);
+                    continue main;
+    
+                } else if (textArr[i] === '+' || textArr[i] === '-') {
+                    continue;
+                }
+            }
+    
+            for (let i = 0; i < textArr.length; i++) {
+    
+                if (textArr[i] === '+' || textArr[i] === '-') {
+                    let total = operate(textArr[i], parseFloat(textArr[i - 1]), parseFloat(textArr[i + 1]));
+                    textArr.splice((i - 1), 3, total);
+                    console.log(textArr);
+                    continue main;
+    
+                } else if (textArr[i] === '/' || textArr[i] === '*') {
+                    continue;
+                }
+            }
+        }    
+        displayVal.innerText = textArr[0].toFixed(6);
     }
     if (event.keyCode == 110) {
         if (displayVal.innerText.search(/[.]/) == -1) {
@@ -245,6 +216,7 @@ document.onkeydown = function(event) {
     }   
     if (event.keyCode == 8) {
         displayVal.innerText = '';
+        displayTop.innerText = '';
         firstVar = undefined;
         secondVar = undefined;
         totalVar = undefined;
